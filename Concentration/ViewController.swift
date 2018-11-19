7692e3799be40c8fcb89582c0a01a2b61d73c553
Guga Gongadze <ggong13@freeuni.edu.ge>
2018-11-19 20:23:03 +0400

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Guga Gongadze on 11/19/18.
+//  Copyright © 2018 Guga Gongadze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
