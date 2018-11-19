a328fd32a36a155e2f82c09bba4d5eb698376386
Guga Gongadze <ggong13@freeuni.edu.ge>
2018-11-19 22:08:36 +0400

    Refactor: make code more modular

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices = ["🎃", "👻", "🎃", "👻"]
     
     @IBAction func touchCard(_ sender: UIButton) {
-        flipCard(withEmoji: "👻", on: sender)
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender )
+        }
     }
     
     func flipCard(withEmoji emoji: String, on button: UIButton) {
