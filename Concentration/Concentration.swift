25880767f4d9f0e6bee9ad55155eea5ea05b468a
Guga Gongadze <ggong13@freeuni.edu.ge>
2018-11-20 18:29:14 +0400

    Feat: check if cards match on touch

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -9,13 +9,31 @@
 import Foundation
 
 class Concentration {
+    
     var cards = [Card]()
     
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
     func chooseCard(at index: Int) {
-        if cards[index].isFaceUp {
-            cards[index].isFaceUp = false
-        } else {
-            cards[index].isFaceUp = true
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                
+                // Check if cards match
+                if cards[matchIndex].indentifier == cards[index].indentifier  {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                // either no cards or 2 card are face up
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
         }
     }
     
